3755e19e31663baf22bc497ff2bb0c0c4e157778
Prathamesh Kulkarni <65341679+Prathamesh-Kulkarni01@users.noreply.github.com>
2023-08-25 17:55:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,25 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+btn.addEventListener('click', () => {
+	try{
+		let promises=images.map((img)=>{
+		return fetch(img.url)
+	})
+	alert("promises")
+	Promise.all(promises).then(blobs=>{
+		blobs.map(blob=>{
+			console.log(blob)
+			 let img = document.createElement("img");
+        img.src = blob.url;
+        img.style.margin = '10px';
+        output.appendChild(img);
+		})
+		
+	})
+	}.catch(err=>{
+	console.log(`Failed to load image's URL: ${image.url}`)
+	})
+	
+})
